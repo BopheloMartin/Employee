a28131ccb15a51eba22a1bccad08ce05564e4889
Bophelo <martinbophelo@gmail.com>
2022-09-06 14:45:15 -0700

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,74 @@
+const employeesData = [
+    {
+        id: 1,
+        firstName: 'Bophelo',
+        lastName: 'Martin',
+        salary: '50 000',
+        date: '2021-04-11'
+    },
+    {
+        id: 2,
+        firstName: 'Bianca',
+        lastName: 'Tlhatsoane',
+        salary: '50 000',
+        date: '2020-04-11'
+    },
+    {
+        id: 3,
+        firstName: 'Bianca',
+        lastName: 'Tlhatsoane',
+        salary: '50 000',
+        date: '2020-04-11'
+    },
+    {
+        id: 4,
+        firstName: 'Bianca',
+        lastName: 'Tlhatsoane',
+        salary: '50 000',
+        date: '2020-04-11'
+    },
+    {
+        id: 5,
+        firstName: 'Bianca',
+        lastName: 'Tlhatsoane',
+        salary: '50 000',
+        date: '2020-04-11'
+    },
+    {
+        id: 6,
+        firstName: 'Bianca',
+        lastName: 'Tlhatsoane',
+        salary: '50 000',
+        date: '2020-04-11'
+    },
+    {
+        id: 7,
+        firstName: 'Bianca',
+        lastName: 'Tlhatsoane',
+        salary: '50 000',
+        date: '2020-04-11'
+    },
+    {
+        id: 8,
+        firstName: 'Bianca',
+        lastName: 'Tlhatsoane',
+        salary: '50 000',
+        date: '2020-04-11'
+    },
+    {
+        id: 9,
+        firstName: 'Bianca',
+        lastName: 'Tlhatsoane',
+        salary: '50 000',
+        date: '2020-04-11'
+    },
+    {
+        id: 10,
+        firstName: 'Bianca',
+        lastName: 'Tlhatsoane',
+        salary: '50 000',
+        date: '2020-04-11'
+    },
+];
+
+export { employeesData }
